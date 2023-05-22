2d0ac9edcf2b46a9fef561049a7bf90f1198e54c
Ryan Loh <ryanloh88@ymail.com>
2023-05-22 16:55:31 +0800

    creating new print called papa

--- test.go
+++ test.go
@@ -5,5 +5,5 @@ package main //creates a executable file, has to be same as <filename>
 import "fmt" //library to format strings and print lines 
 
 func main() { //entry point of the file, has to be called main, every file has to have main
-	fmt.Println("Hello, ni de mama")
+	fmt.Println("Hello, ni de papa")
 }
