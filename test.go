9105a04361cb1dce96d7e2c84674d2395be44e1c
Ryan Loh <ryanloh88@ymail.com>
2023-05-24 17:46:38 +0800

    readme and kong api gateway setup

--- test.go
+++ test.go
@@ -7,3 +7,27 @@ import "fmt" //library to format strings and print lines
 func main() { //entry point of the file, has to be called main, every file has to have main
 	fmt.Println("Hello, ni de dog")
 }
+
+
+//flow of project (admin)
+
+//1. create a service for the user to connect to 
+
+// curl -i -s -X POST http://localhost:8001/services \
+//   --data name=example_service \
+//   --data url='http://mockbin.org'
+
+
+//2. 
+
+
+
+
+//flow of project (user)
+
+//1. send http request to gateway
+//2. 
+
+
+
+// curl -X GET http://localhost:8000/mock/requests
