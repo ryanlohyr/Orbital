5c8266856cbdf60cde3d9430f4dc86c6688a8e07
isaacchin12 <isaacchin12@gmail.com>
2023-06-07 22:42:53 +0800

    created kitex folder

--- test.go
+++ test.go
@@ -7,4 +7,3 @@ import "fmt" //library to format strings and print lines
 func main() { //entry point of the file, has to be called main, every file has to have main
 	fmt.Println("Hello, ni de dog")
 }
-
