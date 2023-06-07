da6d48058a130eaa4c28e1c69ff83d2bb516cf1c
Ryan Loh <ryanloh88@ymail.com>
2023-06-07 22:33:42 +0800

    post request hertz created

--- test.go
+++ test.go
@@ -8,26 +8,3 @@ func main() { //entry point of the file, has to be called main, every file has t
 	fmt.Println("Hello, ni de dog")
 }
 
-
-//flow of project (admin)
-
-//1. create a service for the user to connect to 
-
-// curl -i -s -X POST http://localhost:8001/services \
-//   --data name=example_service \
-//   --data url='http://mockbin.org'
-
-
-//2. 
-
-
-
-
-//flow of project (user)
-
-//1. send http request to gateway
-//2. 
-
-
-
-// curl -X GET http://localhost:8000/mock/requests
