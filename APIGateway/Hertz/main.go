2590c5f99adb5f542d3fd5fc7c6f7596de356905
Ryan Loh <ryanloh88@ymail.com>
2023-06-15 11:34:29 +0800

    API Gateway and RPC Backend

---
+++ APIGateway/Hertz/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/client/genericclient"
+	"github.com/cloudwego/kitex/pkg/generic"
+)
+
+//run command in command line: go build -o hertz_demo && ./hertz_demo
+//to test endpoint: curl http://127.0.0.1:8888/ping
+
+func makeThriftCall(IDLPath string, response []byte, requestURL string, ctx context.Context) (interface{},  error)  {
+	p, err := generic.NewThriftFileProvider(IDLPath)
+	if err != nil {
+		fmt.Println("error creating thrift file provider")
+		return 0, err
+	}
+	g, err := generic.HTTPThriftGeneric(p)
+	if err != nil {
+		return 0, errors.New(("error creating thrift generic"))
+	} 
+	cli, err := genericclient.NewClient("Hello", g,client.WithHostPorts("0.0.0.0:8888"))
+
+
+	if err != nil {
+		return 0, errors.New(("invalid client name"))
+	}
+
+	req, err := http.NewRequest(http.MethodGet, requestURL, bytes.NewBuffer(response))
+	req.Header.Set("token", "1")
+	if err != nil {
+		fmt.Println("error constructing req")
+		return 0, err
+	}
+
+	customReq, err := generic.FromHTTPRequest(req)
+
+	if err != nil {
+		fmt.Println("error creating custom req")
+		return 0, err
+	}
+
+	fmt.Println(customReq)
+
+	resp, err := cli.GenericCall(ctx, "hello", customReq)
+
+	fmt.Println("generic call successful")
+	fmt.Println(resp)
+
+	if err != nil {
+		fmt.Println("error making generic call")
+		return 0,err
+	}
+
+	realResp := resp.(*generic.HTTPResponse)
+	
+    return realResp, nil
+}
+
+func main() {
+	//we can use server.WithHostPorts as it returns a type of config.option
+	h := server.Default(server.WithHostPorts("0.0.0.0:8881")) 
+
+	h.GET("/ping", func(ctx context.Context, c *app.RequestContext) {
+		c.JSON(consts.StatusOK, utils.H{"message": "hello from ryan"})
+	})
+
+	h.GET("/get", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "get")
+		// c.AbortWithStatus(300)
+	})
+
+	h.POST("/post", func(ctx context.Context, c *app.RequestContext) {
+        //url to send request to??
+		var requestUrl string = "http://example.com/life/client/11?vint64=1&items=item0,item1,itme2"
+		var IDLPath string = "./hello.thrift"
+		var jsonData map[string]interface{}
+
+        //returns data in an array of bytes
+		response := c.GetRawData() 
+
+		//converts the array of bytes into array format and loads it into jsonData
+		err := json.Unmarshal(response, &jsonData)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			c.String(consts.StatusBadRequest, "bad post request")
+			return
+		}
+
+		//whatever the key value is,  has to be consistent with backend
+		dataValue, ok := jsonData["text"].(string) 
+
+        fmt.Println("message is " + dataValue)
+
+		if !ok {
+			c.String(consts.StatusBadRequest, "data provided was not a string")
+			return
+		}
+		
+		responseFromRPC, err := makeThriftCall(IDLPath, response, requestUrl,ctx)
+
+        if(err != nil){
+			fmt.Println(err)
+			c.String(consts.StatusBadRequest, "error in thrift call ")
+			return
+        }
+
+		fmt.Println("Post Request successful")
+		
+		c.JSON(consts.StatusOK,responseFromRPC)
+
+	})
+
+	h.PUT("/put", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "put")
+	})
+	h.DELETE("/delete", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "delete")
+	})
+	h.PATCH("/patch", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "patch")
+	})
+
+	//spin runs the application
+	h.Spin()
+}
+
+//converts json into []bytes
+//jsonBytes := []byte(`{"data":"helloworld"}`)
--- Kitex/main.go
+++
@@ -1,9 +0,0 @@
-//To run the file type: go run <filename>.go
-
-package main //creates a executable file, has to be same as <filename>
-
-import "fmt" //library to format strings and print lines
-
-func main() { //entry point of the file, has to be called main, every file has to have main
-	fmt.Println("Hello again")
-}
